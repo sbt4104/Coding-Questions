364ecf4bdeb2783115887e2de45e85f8a8b732d3
sbt4104 <sthorat661@gmail.com>
2022-05-20 01:44:34 +0530

    longest substring with same letter after replacement

---
+++ longest_substring_with_same_letter_after_replacement.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    private:
+        string input;
+        int k;
+    public:
+        void getInput(string str, int k) {
+            input=str;
+            this->k = k;
+        }
+        int longestSubstring() {
+            int ans=INT_MIN, maxFrequency=INT_MIN, start=0;
+            unordered_map<char, int> countFrequency;
+
+            for(int end=0; end<input.length(); end++) {
+                countFrequency[input[end]]++;
+                maxFrequency = max(maxFrequency, countFrequency[input[end]]);
+
+                if(end-start+1-maxFrequency > k) {
+                    countFrequency[input[start]]--;
+                    start++;
+                }
+
+                ans = max(ans,end-start+1);
+            }
+            return ans;
+        }
+};
+
+int main() {
+    Solution obj;
+    obj.getInput("aaabd",1);
+    cout<<"answer: "<<obj.longestSubstring()<<endl;
+    return 0;
+}
