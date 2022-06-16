8d73156724f7379cdb88ffe28aedeba91698e6af
sbt4104 <sthorat661@gmail.com>
2022-06-16 22:13:02 +0530

    sliding_window_median template

---
+++ sliding_window_median.cpp
@@ -0,0 +1,19 @@
+/*
+Given an array of numbers and a number ‘k’, find the median of all the ‘k’ sized sub-arrays (or windows) of the array.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+class MedianOfWindow{
+    public:
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    vector<float> findSlidingWinMedian(vector<int> input, int k) {
+
+    }
+};
+
+int main() {
+    // Figure out a way to implement remove operation in stl heap
+    return 0;
+}
