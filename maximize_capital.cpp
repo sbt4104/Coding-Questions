5557e4528f05efb44540547585fdf45b167ec54c
sbt4104 <sthorat661@gmail.com>
2022-06-17 23:07:11 +0530

    maximize_capital

---
+++ maximize_capital.cpp
@@ -0,0 +1,22 @@
+/*
+Given a set of investment projects with their respective profits, we need to find the most profitable projects. We are given an initial capital and are allowed to invest only in a fixed number of projects. Our goal is to choose projects that give us the maximum profit.
+
+We can start an investment project only when we have the required capital. Once a project is selected, we can assume that its profit has become our capital.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+class MaximiseCapital{
+    public:
+    int maximiseCapital(vector<int> &capitals, vector<int> &profits, int initialCapital, int numProjects) {
+        int maxCapital=0;
+        return maxCapital;
+    }
+};
+
+int main() {
+    MaximiseCapital obj;
+    obj.maxCapital({0,1,2}, {1,2,3}, 1, 2);
+    // work on the solution
+    return 0;
+}
